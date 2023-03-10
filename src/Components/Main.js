efb80ad2e6d4dca756a3f3c3a1cf4ab4d709cfb6
Prithviraj <37-prithviraj@users.noreply.gitlab.codify.tech>
2023-03-10 23:55:39 +0530

    components

---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HotelInfo from './HotelInfo'
+import Welcome from './Welcome'
+
+export default function Main() {
+    return (
+        <main id="wrapper">
+            <Welcome />
+            <HotelInfo />
+        </main>
+    )
+}
